176575a201320b6344f3d128fd6d417f0fd845bd
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-23 20:45:18 +0200

    Added Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+
+import Login from './components/Login';
 
 const App = () => {
-  return <div>App</div>;
+  const [id, setId] = useState();
+
+  return (
+    <Fragment>
+      {id}
+      <Login onIdSubmit={setId} />
+    </Fragment>
+  );
 };
 
 export default App;
