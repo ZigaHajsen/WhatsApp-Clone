7260dcaf66501cadb5bf1e2ac4134ddf0edf499f
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-24 12:48:43 +0200

    Added OpenConversation component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { useConversations } from '../contexts/ConversationsProvider';
+
 import Sidebar from './Sidebar';
+import OpenConversation from './OpenConversation';
 
 const Dashboard = ({ id }) => {
+  const { selectedConversation } = useConversations();
+
   return (
     <div className='d-flex' style={{ height: '100vh' }}>
       <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
     </div>
   );
 };
---
+++ client/src/components/OpenConversation.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import { Button, Form, InputGroup } from 'react-bootstrap';
+
+const OpenConversation = () => {
+  const [text, setText] = useState('');
+
+  return (
+    <div className='d-flex flex-column flex-grow-1'>
+      <div className='flex-grow-1 overflow-auto'></div>
+      <Form>
+        <Form.Group className='m-2'>
+          <InputGroup>
+            <Form.Control
+              as='textarea'
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: '75px', resize: 'none' }}
+            />
+            <InputGroup.Append>
+              <Button type='submit'>Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+export default OpenConversation;
