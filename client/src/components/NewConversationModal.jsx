4a6f34d5e422c16ae6e31784047ab78971387078
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-24 09:50:36 +0200

    Added conversations modal

--- client/src/components/NewConversationModal.jsx
+++ client/src/components/NewConversationModal.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
-const NewConversationModal = () => {
-  return <div></div>;
+import { useContacts } from '../contexts/ContactsProvider';
+
+import { Modal, Button, Form } from 'react-bootstrap';
+
+const NewConversationModal = ({ closeModal }) => {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    closeModal();
+  };
+
+  const handleCheckboxChange = (contactId) => {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevSelectedContactIds, contactId];
+      }
+    });
+  };
+
+  return (
+    <Fragment>
+      <Modal.Header closeButton>Create Conversation</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controlId={contact.id} key={contact.id}>
+              <Form.Check
+                type='checkbox'
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckboxChange(contact.id)}
+              />
+            </Form.Group>
+          ))}
+          <Button type='submit'>Create</Button>
+        </Form>
+      </Modal.Body>
+    </Fragment>
+  );
 };
 
 export default NewConversationModal;
